0aa2301f4552628b322d4d6746b10fe2b9fb2bcb
Ted Satcher <ted.satcher@dr-inc.com>
2020-09-17 17:13:57 -0500

    Skeleton for problem 3

--- Probs1to100/app/Main.hs
+++ Probs1to100/app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Prob1
 import Prob2
+import Prob3
 
 main :: IO ()
 main = do
   run1
   run2
+  run3
---
+++ Probs1to100/src/Prob3.hs
@@ -0,0 +1,4 @@
+module Prob3 where
+
+run3 :: IO ()
+run3 = "Not solved"
