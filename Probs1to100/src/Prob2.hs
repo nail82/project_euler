c6b29ccfa4dc3922990011fe3dc84126d4907abf
Ted Satcher <ted.satcher@dr-inc.com>
2020-09-17 11:28:36 -0500

    WIP on problem 2

--- Probs1to100/src/Prob2.hs
+++ Probs1to100/src/Prob2.hs
@@ -1,7 +1,9 @@
-module Prob2
-    (
-     run2
-    ) where
+module Prob2 where
+
+{-
+By considering the terms in the Fibonacci sequence whose values do not
+exceed four million, find the sum of the even-valued terms.
+-}
 
 run2 :: IO ()
 run2 = do
@@ -9,4 +11,12 @@ run2 = do
   putStrLn $ show ans
 
 ans :: Int
-ans = 1
+ans = undefined
+
+sumHelper :: (Int, Int, Int) -> (Int, Int, Int)
+sumHelper (nm2, nm1, s) =
+    let c = nm2 + nm1
+        s' = case mod c 2 of
+               0 -> s+c
+               1 -> s
+    in (nm1, c, s')
