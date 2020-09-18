79fa7c6a3c51f0f79a682004a9f9966b01647e2d
Ted Satcher <ted.satcher@dr-inc.com>
2020-09-18 16:29:35 -0500

    Added vector as a dependency.
    Working on seive of Eratosthenes to
    find primes.

--- Probs1to100/src/Prob3.hs
+++ Probs1to100/src/Prob3.hs
@@ -1,4 +1,38 @@
 module Prob3 where
 
+import Data.Vector
+
+{-
+  What is the largest prime factor of the number 600851475143?
+-}
+
+bigOne = 600851475143
+smallOne = 13195
+
 run3 :: IO ()
-run3 = "Not solved"
+run3 = putStrLn "Problem 3 => Not Solved"
+
+data Mark =
+    Unmarked | Marked
+    deriving (Eq, Show)
+
+{-
+I was trying a newtype hear to fool around with it.  It
+started getting unwieldy so I dropped back to a tuple.
+
+newtype MyNum =
+    MyNum (Int, Mark)
+    deriving (Eq, Show)
+-}
+
+markIt :: (Int, Mark) -> (Int, Mark)
+markIt (x,_) = (x,Marked)
+
+sieve :: Int -> Vector (Int, Mark) -> Vector (Int, Mark)
+sieve i ms =
+    let (x,_) = ms ! i
+
+    in ms
+
+makeMyNum :: Int -> (Int, Mark)
+makeMyNum n = (n, Unmarked)
