ae0c9a8ff45229df85f90c61bbef5b2e8455d25b
tsatcher <ted.satcher@dr-inc.com>
2020-09-20 09:47:41 -0500

    Cleanup some warnings.

--- Probs1to100/src/Prob2.hs
+++ Probs1to100/src/Prob2.hs
@@ -9,20 +9,26 @@ By considering the terms in the Fibonacci sequence whose values do not
 exceed four million, find the sum of the even-valued terms.
 -}
 
+fourMil :: Int
+fourMil = 4000000
+
+startState :: (Int, Int, Int)
+startState = (1,2,2)
+
 run2 :: IO ()
 run2 = do
   putStr "Problem 2 => "
-  putStrLn $ show $ ans [3..] startState
+  putStrLn $ show $ ans [1..] startState
 
 ans :: [Int] -> (Int, Int, Int) -> Int
 ans [] (_, _, s) = s
 ans (_:xs) (nm2, nm1, s) =
-    case (fromIntegral nm1) > 4.0e6 of
+    case nm1 > fourMil of
       True -> s
       False -> let t' = sumHelper (nm2, nm1, s)
                in ans xs t'
 
-startState = (1,2,2)
+
 
 sumHelper :: (Int, Int, Int) -> (Int, Int, Int)
 sumHelper (nm2, nm1, s) =
