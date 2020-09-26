463b91ad41a60db3b12eebde204cd2991819ef76
tsatcher <ted.satcher@dr-inc.com>
2020-09-26 13:58:43 -0500

    Problem 3 solved.  Skeleton for 4.

--- Probs1_10/src/Prob3.hs
+++ Probs1_10/src/Prob3.hs
@@ -2,13 +2,11 @@ module Prob3 where
 
 import qualified GHC.Float as F
 import qualified Data.Set as S
-import Data.Int
 import qualified Data.Vector.Unboxed as U
-import Data.Vector.Unboxed ((!), (!?), (//))
+import Data.Vector.Unboxed ((!), (//))
 
 {-
   What is the largest prime factor of the number 600851475143?
-                                                   8462696833
 -}
 
 -- Sieving isn't going to work, bigOne is too big
@@ -17,13 +15,46 @@ bigOne :: Int
 bigOne = 600851475143
 
 smallOne :: Int
-smallOne = 13195
+smallOne = 15015
 
 run3 :: IO ()
 run3 = do
-  let v = sieve $ U.enumFromTo 0 10000
-  print $ U.length v
+  let ans = S.findMax $ fermatFactors bigOne
+  putStr "Problem 3 => "
+  putStrLn $ show ans
 
+-- A table of squares.  Could do this with a call to sqrt
+squares :: S.Set Int
+squares = S.fromList [(x^2) :: Int | x <- [0..ceiling $ sqrt (fromIntegral bigOne) + 1]]
+
+fermatFactors :: Int -> S.Set Int
+fermatFactors n =
+    let t1 = singleFactor n
+    in S.fromList $ fst <$> fermatLoop t1 []
+
+fermatLoop :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+fermatLoop t xs
+    | snd t == 1 = t : xs
+    | otherwise  =    (fermatLoop (singleFactor $ fst t) xs)
+                   <> (fermatLoop (singleFactor $ snd t) xs)
+
+
+singleFactor :: Int -> (Int, Int)
+singleFactor 0 = (0,1)
+singleFactor 1 = (1,1)
+singleFactor 2 = (2,1)
+singleFactor n =
+    let a = ceiling $ sqrt $ fromIntegral n
+    in go a n
+        where
+          go a' n = case S.member b2 squares of
+                      True -> let rhs = F.double2Int $ (F.int2Double a') - (sqrt $ fromIntegral b2)
+                              in (n `div` rhs, rhs)
+                      False -> go (a'+1) n
+              where b2 = a'^2 - n
+
+
+-- Fooling around with sieving
 trialDiv :: Int -> Maybe Int
 trialDiv a = safeHead [x | x <- [2..a-1], mod a x == 0]
 
@@ -59,21 +90,3 @@ sieve v =
 mygcd :: Int -> Int -> Int
 mygcd a 0 = a
 mygcd a b = gcd b $ a `rem` b
-
-squares = S.fromList [(x^2) :: Int | x <- [2..ceiling $ sqrt (fromIntegral bigOne) + 1]]
-
-fermatFactor :: Int -> Int
-fermatFactor n =
-    let a = ceiling $ sqrt $ fromIntegral n
-    in fermatLoop a n
-
-fermatLoop :: Int -> Int -> Int
-fermatLoop a n =
-    case S.member b2 squares of
-      True -> F.double2Int $ (F.int2Double a) - (sqrt $ fromIntegral b2)
-      False -> fermatLoop (a+1) n
-    where b2 = a^2 - n
-
--- a = (ceiling $ sqrt $ fromIntegral bigOne) :: Int
--- b2 = a^2 - bigOne
--- a' = (F.int2Double a) - (sqrt $ fromIntegral b2)
