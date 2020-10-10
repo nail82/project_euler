be36972acf60d215622f3f6c017fe9a3badf6073
tsatcher <ted.satcher@dr-inc.com>
2020-10-10 14:40:40 -0500

    Problem statement for problem 9

--- Probs1_10/src/Prob9.hs
+++ Probs1_10/src/Prob9.hs
@@ -1,5 +1,14 @@
 module Prob9 where
 
+{-
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+There is one triple where a + b + c = 1000.
+
+Find it.
+-}
+
 run9 :: IO ()
 run9 = do
   putStr "Problem 9 => "
