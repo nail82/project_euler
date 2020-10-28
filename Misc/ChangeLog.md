b73b858833945a353340b72f6974966677e74073
Ted Satcher <ted.satcher@dr-inc.com>
2020-10-28 08:19:02 -0500

    Don't need misc project.

--- Misc/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for Misc
-
-## Unreleased changes
--- Misc/app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import System.IO
-import Newton
-
-main :: IO ()
-main = do
-  let t = doIt (0, 4.4)
-  putStrLn $ show t
