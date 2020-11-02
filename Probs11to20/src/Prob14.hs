8ff8bceb5ec314e8efd20a35e3f0ab1b76f0e3e7
Ted Satcher <ted.satcher@dr-inc.com>
2020-11-02 16:33:52 -0600

    Implemented collatz, but not solved.

--- Probs11to20/src/Prob14.hs
+++ Probs11to20/src/Prob14.hs
@@ -4,3 +4,11 @@ run14 :: IO ()
 run14 = do
   putStr "Problem 14 => "
   putStrLn "not solved"
+
+collatz :: [Int] -> [Int]
+collatz [] = []
+collatz (1:xs) = reverse $ 1 : xs
+collatz (x:xs) = let x' = case even x of
+                            True -> x `div` 2
+                            False -> 3 * x + 1
+                 in collatz $ x' : x : xs
