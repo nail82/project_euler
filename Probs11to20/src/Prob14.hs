2944f54388fe166318ef0398f0ac0044813bbf84
Ted Satcher <ted.satcher@dr-inc.com>
2020-11-04 17:00:33 -0600

    Fooling around with memoizing collatz

--- Probs11to20/src/Prob14.hs
+++ Probs11to20/src/Prob14.hs
@@ -1,8 +1,27 @@
-module Prob14
+module Prob14 where
+{--
     (
      run14
     )
     where
+--}
+
+import qualified Data.IntMap.Strict as M
+
+{-
+  Reading the approved solution, it is better to memoize collatz
+  vice computing each chain.
+
+  See here:  wiki.haskell.org/Memoization for some ideas.
+
+  Memoizing is trickier because collatz n+1 doesn't necessarily
+  mean we have already visited collatz n, n-1, .. 1.
+
+  Also, there are some heuristics that can be applied to speed
+  things along.  One is that collatz n = 1 + collatz n/2, when n is even.
+
+  Better just to compute the sum vice creating the whole chain.
+-}
 
 import System.IO
 import qualified Data.List as L
@@ -32,6 +51,16 @@ collatz' (x:xs) = let x' = case even x of
                              False -> 3 * x + 1
                   in collatz' $ x' : x : xs
 
+collatz'' :: Int -> Int
+collatz'' 1 = 0
+collatz'' x = let x' = case even x of
+                         True -> x `div` 2
+                         False -> 3 * x + 1
+              in 1 + collatz'' x'
+
+memCollatz :: Int -> Int
+memCollatz = undefined
+
 collatzData :: [Int] -> [(Int, Int)]
 collatzData ns = fmap go ns
     where go n = (n, length $ collatz n)
@@ -45,3 +74,12 @@ writeData = do
   fh <- openFile fnm WriteMode
   hPutStrLn fh mylines
   hClose fh
+
+collatzTable :: M.IntMap Int
+collatzTable = let t = collatz 999999
+                   alist = mapIt t []
+               in M.fromList alist
+    where
+      mapIt [] ys = ys
+      mapIt (1:[])  ys = (1,0) : ys
+      mapIt (x:xs) ys = (x, length xs) : mapIt xs ys
