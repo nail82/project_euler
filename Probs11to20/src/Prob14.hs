bb06a9119a9c9c501132cb544ebd90b038eebb5f
Ted Satcher <ted.satcher@dr-inc.com>
2020-11-05 14:51:44 -0600

    Clean up problem 14 a bit.
    Not memoed yet, but cleaner.

--- Probs11to20/src/Prob14.hs
+++ Probs11to20/src/Prob14.hs
@@ -32,24 +32,21 @@ run14 = do
   putStrLn $ show ans14
 
 ans14 :: Int
-ans14 = let d = collatzData [1..999999]
-            m = foldr maxTup (0,0) d
-        in fst m
+ans14 = snd $ M.findMax $ makeCollatzMap [500000..999999] $ M.singleton 0 1
 
 
-maxTup :: (Int, Int) -> (Int, Int) -> (Int, Int)
-maxTup l r = if snd l >= snd r then l else r
+makeCollatzMap :: [Int] -> M.IntMap Int -> M.IntMap Int
+makeCollatzMap [] m = m
+makeCollatzMap (x:xs) m = let t = collatzTup x
+                              m' = M.insertWith max (fst t) (snd t) m
+                          in makeCollatzMap xs m'
 
-collatz :: Int -> [Int]
-collatz n = collatz' [n]
+collatzTup :: Int -> (Int, Int)
+collatzTup n = (collatz'' n, n)
 
-collatz' :: [Int] -> [Int]
-collatz' [] = []
-collatz' (1:xs) = reverse $ 1 : xs
-collatz' (x:xs) = let x' = case even x of
-                             True -> x `div` 2
-                             False -> 3 * x + 1
-                  in collatz' $ x' : x : xs
+invCollatzTup :: Int -> (Int, Int)
+invCollatzTup n = let t = collatzTup n
+                  in (snd t, fst t)
 
 collatz'' :: Int -> Int
 collatz'' 1 = 0
@@ -58,12 +55,9 @@ collatz'' x = let x' = case even x of
                          False -> 3 * x + 1
               in 1 + collatz'' x'
 
-memCollatz :: Int -> Int
-memCollatz = undefined
 
 collatzData :: [Int] -> [(Int, Int)]
-collatzData ns = fmap go ns
-    where go n = (n, length $ collatz n)
+collatzData = fmap invCollatzTup
 
 -- A function to take a look at the data
 writeData :: IO ()
@@ -74,12 +68,3 @@ writeData = do
   fh <- openFile fnm WriteMode
   hPutStrLn fh mylines
   hClose fh
-
-collatzTable :: M.IntMap Int
-collatzTable = let t = collatz 999999
-                   alist = mapIt t []
-               in M.fromList alist
-    where
-      mapIt [] ys = ys
-      mapIt (1:[])  ys = (1,0) : ys
-      mapIt (x:xs) ys = (x, length xs) : mapIt xs ys
