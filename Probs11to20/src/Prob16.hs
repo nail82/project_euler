cf723a47c4183abe9a0514107570d6c6277c0c99
tsatcher <ted.satcher@dr-inc.com>
2020-11-25 08:37:51 -0600

    Added remainder of problem statements for 11to20.

--- Probs11to20/src/Prob16.hs
+++ Probs11to20/src/Prob16.hs
@@ -1,5 +1,11 @@
 module Prob16 where
 
+{-
+  2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+  What is the sum of the digits of the number 2^1000?
+-}
+
 run16 :: IO ()
 run16 = do
   putStr "Problem 16 => "
