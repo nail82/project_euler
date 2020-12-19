b2c805ea86550d468d461390f4f38efb5bf5fbca
tsatcher <ted.satcher@dr-inc.com>
2020-12-19 11:44:42 -0600

    Problem 19 solution

--- Probs11to20/src/Prob19.hs
+++ Probs11to20/src/Prob19.hs
@@ -18,7 +18,29 @@ module Prob19 where
   century (1 Jan 1901 to 31 Dec 2000)?
 -}
 
+import Data.Time
+
 run19 :: IO ()
 run19 = do
   putStr "Problem 19 => "
-  putStrLn "not solved"
+  putStrLn $ show ans19
+
+ans19 :: Int
+ans19 = sum $ fmap (sundayIndicator . myAddMonth) [0..1199]
+
+dayOne :: Maybe Day
+dayOne = fromGregorianValid 1901 1 1
+
+sundayIndicator :: Maybe Day -> Int
+sundayIndicator Nothing = 0
+sundayIndicator jd = let jdow = fmap dayOfWeek jd
+                     in case jdow of
+                          Just Sunday -> 1
+                          _ -> 0
+
+addMonth :: Maybe Day -> Integer -> Maybe Day
+addMonth Nothing _ = dayOne
+addMonth (Just d) n = Just $ addGregorianMonthsClip n d
+
+myAddMonth :: Integer -> Maybe Day
+myAddMonth = addMonth dayOne
