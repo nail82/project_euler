e1e7d17411ebf226560b22198512d8916032fecd
Ted Satcher <ted.satcher@gmail.com>
2022-05-03 10:41:29 -0500

    Finished linear solution to Prob24

--- Probs21to30/src/Prob24.hs
+++ Probs21to30/src/Prob24.hs
@@ -14,8 +14,7 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2,
 
 import qualified Data.List as L
 import qualified Data.Vector as V
-import Data.Vector ((//))
-import Data.Vector ((!))
+import Data.Vector ((//), (!))
 
 -- Recursive solution
 -- Insert an element at the ends and between elements of a list
@@ -40,21 +39,26 @@ permuteList' (x:xs) xxs =
 permuteList :: (Ord a) => [a] -> [[a]]
 permuteList xs = permuteList' xs [[]]
 
+showAns :: [Int] -> String
+showAns = foldr (\x s -> show x <> s) ""
+
 ans24 :: String
 ans24 =
-    let showAns = foldr (\x s -> show x <> s) ""
-        xs = [0..9] :: [Int]
+    let xs = [0..9] :: [Int]
         n = L.product [1..length xs]
         perms = permuteList xs
         ans = snd $ head $ L.dropWhile (\t -> fst t < 1_000_000) (zip [1..n] perms)
      in showAns ans
+
+
+
 -- End recursive solution
 
 -- Linear solution
 {--
 Here is a linear solution from Knuth.
 Assumes a sorted list `a' with n sorted elements indexed from 1 to n.
-Also assumes an element and index 0 that is strictly less than a_n.
+Also assumes an element at index 0 that is strictly less than a_n.
 This will produce permutations in lexicographic order.
 
 let j = n-1
@@ -82,18 +86,19 @@ while (k < l)
 
 findJ :: V.Vector Int -> (Int, V.Vector Int)
 findJ v =
-    let n = (V.length v) - 1
+    let n = V.length v - 1
         go 0 = 0
-        go j
-            | (v ! j) < (v ! (j+1)) = j
-            | otherwise = go (j-1)
+        go j'
+            | (v ! j') < (v ! (j'+1)) = j'
+            | otherwise = go (j'-1)
 
         j = go (n-1)
     in (j,v)
 
+findL :: (Int, V.Vector Int) -> (Int, V.Vector Int)
 findL (0,v) = (0,v)
 findL (j,v) =
-    let l = (V.length v) - 1
+    let l = V.length v - 1
 
         go 0 = 0
         go l'
@@ -111,10 +116,11 @@ swapEm v (i1,i2) =
         a2 = v ! i2
     in v // [(i1,a2), (i2,a1)]
 
+moveK :: (Int, V.Vector Int) -> V.Vector Int
 moveK (0,v) = v
 moveK (j,v) =
     let k = j+1
-        l = (V.length v) - 1
+        l = V.length v - 1
 
         go v' k' l'
            | (k' >= l') = v'
@@ -126,8 +132,20 @@ moveK (j,v) =
 
     in go v k l
 
-onePermute = (moveK . findL . findJ)
+onePermute :: V.Vector Int -> V.Vector Int
+onePermute = moveK . findL . findJ
 
+statePermute :: (Int, V.Vector Int) -> V.Vector Int
+statePermute (1_000_000,v) = v
+statePermute (i,v) =
+    let v' = onePermute v
+    in statePermute (i+1, v')
+
+ans24' :: String
+ans24' =
+    let v = V.fromList [-1..9]
+        (_:ans) = V.toList $ statePermute (1,v)
+    in showAns ans
 
 -- End linear solution
 
