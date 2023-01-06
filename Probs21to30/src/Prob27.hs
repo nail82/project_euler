d60ffad5476bab5dc2c3561ab085313bca5e3fb1
Ted Satcher <ted.satcher@gmail.com>
2023-01-06 12:57:44 -0600

    Added some notes

--- Probs21to30/src/Prob27.hs
+++ Probs21to30/src/Prob27.hs
@@ -32,11 +32,15 @@ Considering quadratics of the form:
 
   n^{2} + an + b,
 
-where  |a| <= 1000 and |b| <= 1000.
+where  |a| < 1000 and |b| <= 1000.
 
 Find the product of the coefficients, a and b, for the quadratic
 expression that produces the maximum number of primes for consecutive
 values of n, starting with 0.
+
+Notes:
+b has to be prime, since it is the solution at n = 0
+b^2 + b(1) + b isn't prime
 
 --}
 
