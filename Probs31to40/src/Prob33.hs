64c972b9ed0b8bd0e64d6ea5e6f7f022490407a7
Ted Satcher <ted.satcher@gmail.com>
2023-05-18 06:53:13 -1000

    Problem statements for 31-33

--- Probs31to40/src/Prob33.hs
+++ Probs31to40/src/Prob33.hs
@@ -1,5 +1,22 @@
 module Prob33 where
 
+{--
+
+The fraction 49/98 is a curious fraction, as an inexperienced
+mathematician in attempting to simplify it may incorrectly believe
+that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction,
+less than one in value, and containing two digits in the numerator and
+denominator.
+
+If the product of these four fractions is given in its lowest common
+terms, find the value of the denominator.
+
+--}
+
 run33 :: IO ()
 run33 = do
   putStr "Problem 33 => "
